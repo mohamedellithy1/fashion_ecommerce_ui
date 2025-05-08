c19c3d862a23bf0714cd5ba4657e9d134876bf7f
mohamedellithy1 <ellithymohamed96@gmail.com>
2025-05-08 12:29:10 +0300

    Add HomeScreen widget with layout and product display

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,98 @@
+import 'package:fashion_ecommerce_ui/components/custom_text.dart';
+import 'package:fashion_ecommerce_ui/core/app_colors.dart';
+import 'package:fashion_ecommerce_ui/components/custom_appbar.dart';
+import 'package:fashion_ecommerce_ui/models/product_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      appBar: CustomAppbar(isBlack: true),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 10,
+            left: 0,
+            right: 0,
+            child: SvgPicture.asset('assets/texts/10.svg', height: 180),
+          ),
+          Positioned(
+            top: 80,
+            left: 0,
+            right: 0,
+            child: SvgPicture.asset('assets/texts/Collection.svg'),
+          ),
+          Positioned(
+            top: 40,
+            left: 0,
+            right: 0,
+            child: SvgPicture.asset('assets/texts/October.svg'),
+          ),
+
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                children: [
+                  Gap(120),
+                  Image.asset('assets/cover/cover1.png'),
+                  Gap(20),
+
+                  GridView.builder(
+                    padding: EdgeInsets.zero,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 0,
+                      crossAxisSpacing: 15,
+                      childAspectRatio: 0.5,
+                      crossAxisCount: 2,
+                    ),
+
+                    itemCount: ProductModel.porducts.length,
+                    itemBuilder: (context, index) {
+                      final item = ProductModel.porducts;
+
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(item[index].image),
+                          Gap(10),
+                          CustomText(text: item[index].name),
+                          Gap(6),
+
+                          CustomText(
+                            text: item[index].description,
+                            color: Colors.grey,
+                          ),
+                          Gap(6),
+                          CustomText(
+                            text: "\$ ${item[index].price.toString()}",
+                            color: Colors.red[700]!,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                  Gap(5),
+                  CustomText(
+                    text: "You may also like".toUpperCase(),
+                    fontSize: 30,
+                  ),
+                  Gap(5),
+                  Image.asset("assets/svcs/line.png", width: 190),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
