24b0dde1071380a7f2694c204233cdbdef0bb0eb
mohamedellithy1 <ellithymohamed96@gmail.com>
2025-05-12 11:19:13 +0300

    Refactor Checkout screen to use CartWidget and update quantity handling

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -1,6 +1,9 @@
+import 'package:fashion_ecommerce_ui/components/app_button.dart';
+import 'package:fashion_ecommerce_ui/components/cart_widget.dart';
 import 'package:fashion_ecommerce_ui/components/custom_appbar.dart';
 import 'package:fashion_ecommerce_ui/components/custom_head.dart';
 import 'package:fashion_ecommerce_ui/components/custom_text.dart';
+import 'package:fashion_ecommerce_ui/screens/placeorder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
@@ -24,84 +27,31 @@ class Checkout extends StatefulWidget {
 }
 
 class _CheckoutState extends State<Checkout> {
-  int quantity = 1;
+  int selectedQuantity = 1;
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: CustomAppbar(isBlack: false),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15),
         child: Column(
           children: [
-            CustomHead(),
+            CustomHead(
+              text: "Checkout",
+            ),
             Gap(20),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(widget.image, height: 190),
-                Gap(20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CustomText(
-                      text: widget.name.toUpperCase(),
-                      spacing: 6,
-                      color: Colors.black,
-                    ),
-                    Gap(10),
-                    SizedBox(
-                      width: size.width * 0.5,
-                      child: CustomText(
-                        maxLines: 3,
-                        text: widget.description,
-                        color: Colors.black,
-                      ),
-                    ),
-                    Gap(20),
-                    Row(
-                      children: [
-                        addAndMinButton(
-                          image: "assets/svcs/min.svg",
-                          onTap: () {
-                            if (quantity == 1) {
-                              return;
-                            } else {
-                              setState(() {
-                                quantity--;
-                              });
-                            }
-                          },
-                        ),
-                        Gap(10),
-                        CustomText(
-                          text: quantity.toString(),
-                          color: Colors.black,
-                        ),
-                        Gap(10),
-
-                        addAndMinButton(
-                          image: "assets/svcs/plus.svg",
-                          onTap: () {
-                            setState(() {
-                              quantity++;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    Gap(20),
-                    CustomText(
-                      text: "\$ ${widget.price.toString()}",
-                      color: Colors.red[700]!,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ],
-                ),
-              ],
+            CartWidget(
+              image: widget.image,
+              description: widget.description,
+              name: widget.name,
+              price: widget.price,
+              onChanged: (number) {
+                setState(() {
+                  selectedQuantity = number;
+                });
+              },
             ),
             Gap(20),
             line(),
@@ -135,7 +85,7 @@ class _CheckoutState extends State<Checkout> {
                 CustomText(text: "Total", color: Colors.black, fontSize: 20),
                 Spacer(),
                 CustomText(
-                  text: "\$ ${widget.price * quantity}",
+                  text: "\$ ${widget.price * selectedQuantity}",
                   color: Colors.red[700]!,
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -143,27 +93,25 @@ class _CheckoutState extends State<Checkout> {
               ],
             ),
             Gap(20),
-            Container(
-              height: 50,
-              width: size.width,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset("assets/svcs/shoppingbag.svg"),
-                    Gap(10),
-                    CustomText(
-                      text: "Checkout",
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ],
-                ),
-              ),
+            AppButton(
+              isSvg: true,
+              title: "Checkout",
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder:
+                        (context) => Placeorder(
+                          quantity: selectedQuantity,
+                          totalPrice: widget.price * selectedQuantity,
+                          image: widget.image,
+                          description: widget.description,
+                          name: widget.name,
+                          price: widget.price,
+                        ),
+                  ),
+                );
+              },
             ),
             Gap(20),
           ],
